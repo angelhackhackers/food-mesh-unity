34eee05349ee16fbb1d3455ea2c959ad78e69a4f
Gravetow <gravetobackup@gmail.com>
2019-06-29 19:05:10 +0200

    Added UI stuff

---
+++ Assets/Rating.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rating : MonoBehaviour
+{
+    public Sprite starEmpty;
+    public Sprite starFull;
+
+    public List<SpriteRenderer> ratingStars = new List<SpriteRenderer>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
